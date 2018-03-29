d473b3a9a47b5a30ab75091c76b83c073b5de3bb
Dima Chizhevsky <dima.chizhevsky@gmail.com>
2018-03-29 13:44:14 +0000

    settings table

--- data/schema.sql
+++ data/schema.sql
@@ -80,6 +80,11 @@ CREATE TYPE job_status AS ENUM (
 	'done' -- successfully executed
 );
 
+CREATE TABLE settings (
+    key text PRIMARY KEY,
+    value jsonb NOT NULL
+);
+
 -- Users are party in distributed trade.
 -- Each of them can play an agent role, a client role, or both of them.
 CREATE TABLE users (

cab604903ae05503cdc690e7324f73859c6f0227
Dima Chizhevsky <dima.chizhevsky@gmail.com>
2018-04-06 02:07:56 +0000

    Billing, RC

--- billing/monitor.go
+++ billing/monitor.go
@@ -1,183 +1,191 @@
 package billing
 
 import (
-	"gopkg.in/reform.v1"
 	"errors"
 	"github.com/privatix/dappctrl/util"
+	"gopkg.in/reform.v1"
 	"time"
 )
 
+// Billing monitor provides logic for checking channels for various cases,
+// in which service(s) must be suspended/terminated/or unsuspended (continued).
+// All conditions are checked on the DB level,
+// so it is safe to call monitors methods from separate goroutines.
 type Monitor struct {
-	interval time.Duration
-	db *reform.DB
+	db     *reform.DB
 	logger *util.Logger
+
+	// Interval between next round checks.
+	interval time.Duration
+
+	// Used only in tests purposes.
+	// By default, in production builds is set to nil.
+	// If not nil - collects IDs of channels, that was selected for further processing.
+	testsSelectedChannelsIDs []string
 }
 
-// todo: docs
-func NewMonitor(interval time.Duration, db *reform.DB, logger * util.Logger) (*Monitor, error) {
+func NewMonitor(interval time.Duration, db *reform.DB, logger *util.Logger) (*Monitor, error) {
 	if db == nil {
 		return nil, errors.New("`db` is required")
 	}
 
-	return &Monitor{interval, db, logger}, nil
+	return &Monitor{db, logger, interval, nil}, nil
 }
 
 func (m *Monitor) Run() error {
 	m.logger.Info("Billing monitor started")
 
 	for {
-		time.Sleep(m.interval)
-		m.logger.Info("Billing checks round started")
-
-		if err := m.callChecksAndReportErrorIfAny(
-			m.VerifySecondsBasedChannels,
-			m.VerifyUnitsBasedChannels,
-			m.VerifyChannelsForInactivity,
-			m.processSuspendedChannels);
-
-			err != nil {
+		if err := m.processRound(); err != nil {
 			return err
 		}
 
-		m.logger.Info("Billing checks round finished")
+		time.Sleep(m.interval)
 	}
 }
 
-// Method is public, so it is possible to call it before providing any service,
-// for example, in prepaid schemes. Also, this method might be called from the Rest API,
-// as it is listed in the billing requirements.
 func (m *Monitor) VerifySecondsBasedChannels() error {
 	// Selects all channels, which
 	// 1. used tokens >= deposit tokens
 	// 2. total consumed seconds >= max offer units (seconds in this case)
 	// Only checks channels, which corresponding offers are using seconds as billing basis.
 	query := `
-		SELECT channels.id::text
-		FROM channels
-		  LEFT JOIN sessions ses ON channels.id = ses.channel
-		  LEFT JOIN offerings offer ON channels.offering = offer.id
-		
-		WHERE
-		  channels.service_status IN ('pending', 'active') AND
-		  offer.unit_type = 'seconds' AND
-		  
-		  (
-		    offer.setup_price + (ses.seconds_consumed * offer.unit_price) >= channels.total_deposit OR
-		    ses.seconds_consumed >= offer.max_unit
-		  )`
+                SELECT channels.id::text
+                FROM channels
+                        LEFT JOIN sessions ses ON channels.id = ses.channel
+                        LEFT JOIN offerings offer ON channels.offering = offer.id
+          
+                WHERE
+                        channels.service_status IN ('pending', 'active') AND
+                        offer.unit_type = 'seconds'
+          
+                GROUP BY channels.id, offer.setup_price, offer.unit_price, offer.max_unit
+                HAVING
+                        (
+				offer.setup_price + 
+				coalesce(sum(ses.seconds_consumed), 0) * offer.unit_price 
+				>= channels.total_deposit
+                        ) OR 
+			(
+				coalesce(sum(ses.seconds_consumed), 0) >= offer.max_unit
+			)`
 
-	return m.processEachChannel(query, m.suspendService)
+	return m.processEachChannel(query, m.terminateService)
 }
 
-// Method is public, so it is possible to call it before providing any service,
-// for example, in prepaid schemes. Also, this method might be called from the Rest API,
-// as it is listed in the billing requirements.
 func (m *Monitor) VerifyUnitsBasedChannels() error {
 	query := `
 		SELECT channels.id::text
-		FROM channels
-		  LEFT JOIN sessions ses ON channels.id = ses.channel
-		  LEFT JOIN offerings offer ON channels.offering = offer.id
-		
-		WHERE
-		  channels.service_status IN ('pending', 'active') AND
-		  offer.unit_type = 'units' AND
-		  
-		  (
-		    offer.setup_price + (ses.units_used * offer.unit_price) >= channels.total_deposit OR
-		    ses.units_used >= offer.max_unit
-		  )`
+                FROM channels
+                        LEFT JOIN sessions ses ON channels.id = ses.channel
+                        LEFT JOIN offerings offer ON channels.offering = offer.id
+          
+                WHERE
+                        channels.service_status IN ('pending', 'active') AND
+                        offer.unit_type = 'units'
+          
+                GROUP BY channels.id, offer.setup_price, offer.unit_price, offer.max_unit
+                HAVING
+                        (
+				offer.setup_price + 
+				coalesce(sum(ses.units_used), 0) * offer.unit_price 
+				>= channels.total_deposit
+                        ) OR 
+			(
+				coalesce(sum(ses.units_used), 0) >= offer.max_unit
+			)`
 
-	return m.processEachChannel(query, m.suspendService);
+	return m.processEachChannel(query, m.terminateService)
 }
-
-// todo: docs
 func (m *Monitor) VerifyBillingLags() error {
 	// Checking billing lags.
 	// All channels, that are not suspended and are not terminated,
 	// but are suffering from the billing lags - must be suspended.
 	query := `
-		SELECT channels.id::text
-		FROM channels
-		  LEFT JOIN sessions ses ON channels.id = ses.channel
-		  LEFT JOIN offerings offer ON channels.offering = offer.id
-		
-		WHERE
-		  channels.service_status IN ('pending', 'active') AND
-		  (
-		    (ses.units_used / offer.billing_interval -
-		      (channels.receipt_balance - offer.setup_price) / offer.unit_price) > offer.max_billing_unit_lag
-		  )`
+		SELECT channels.id :: text
+                FROM channels
+                        LEFT JOIN sessions ses ON channels.id = ses.channel
+                        LEFT JOIN offerings offer ON channels.offering = offer.id
 
-	if err := m.processEachChannel(query, m.suspendService); err != nil {
-		return err
-	}
+                WHERE
+                        channels.service_status IN ('pending', 'active')
 
-	// All channels, that are suspended, but now seems to be payed - must be continued.
-	query = `
-		SELECT channels.id::text
-		FROM channels
-		  LEFT JOIN sessions ses ON channels.id = ses.channel
-		  LEFT JOIN offerings offer ON channels.offering = offer.id
-		
-		WHERE
-		  channels.service_status = 'suspended' AND
-		  (
-		    (ses.units_used / offer.billing_interval -
-		     (channels.receipt_balance - offer.setup_price) / offer.unit_price) <= offer.max_billing_unit_lag  
-		  )`
+                GROUP BY channels.id, offer.billing_interval, offer.setup_price, offer.unit_price, offer.max_billing_unit_lag
+                HAVING
+                        (coalesce(sum(ses.units_used), 0) / offer.billing_interval - (channels.receipt_balance - offer.setup_price) / offer.unit_price) > offer.max_billing_unit_lag OR
+                        (coalesce(sum(ses.seconds_consumed), 0) / offer.billing_interval - (channels.receipt_balance - offer.setup_price) / offer.unit_price) > offer.max_billing_unit_lag;`
 
-	return m.processEachChannel(query, m.continueService)
+	return m.processEachChannel(query, m.unsuspendService)
 }
 
-// Method is public, so it is possible to call it before providing any service,
-// for example, in prepaid schemes. Also, this method might be called from the Rest API,
-// as it is listed in the billing requirements.
+func (m *Monitor) VerifySuspendedChannelsAndTryToUnsuspend() error {
+	// All channels, that are suspended, but now seems to be payed - must be unsuspended.
+	query := `
+		SELECT channels.id :: text
+                FROM channels
+                        LEFT JOIN sessions ses ON channels.id = ses.channel
+                        LEFT JOIN offerings offer ON channels.offering = offer.id
+
+                WHERE
+                        channels.service_status IN ('suspended')
+
+                GROUP BY channels.id, offer.billing_interval, offer.setup_price, offer.unit_price, offer.max_billing_unit_lag
+                HAVING
+                        (coalesce(sum(ses.units_used), 0) / offer.billing_interval - (channels.receipt_balance - offer.setup_price) / offer.unit_price) <= offer.max_billing_unit_lag OR
+                        (coalesce(sum(ses.seconds_consumed), 0) / offer.billing_interval - (channels.receipt_balance - offer.setup_price) / offer.unit_price) <= offer.max_billing_unit_lag;`
+
+	return m.processEachChannel(query, m.unsuspendService)
+}
+
 func (m *Monitor) VerifyChannelsForInactivity() error {
 	query := `
 		SELECT channels.id::text
 		FROM channels
-		  LEFT JOIN sessions ses ON channels.id = ses.channel
-		  LEFT JOIN offerings offer ON channels.offering = offer.id
+			LEFT JOIN sessions ses ON channels.id = ses.channel
+			LEFT JOIN offerings offer ON channels.offering = offer.id
 		
 		WHERE
-		  channels.service_status IN ('pending', 'active')
+			channels.service_status IN ('pending', 'active', 'suspended')
 		GROUP BY channels.id, offer.max_inactive_time_sec
 		HAVING max(ses.last_usage_time) + (offer.max_inactive_time_sec * INTERVAL '1 second') < now()`
 
-	return m.processEachChannel(query, m.suspendService)
+	return m.processEachChannel(query, m.terminateService)
 }
 
-// Method is public, so it is possible to call it before providing any service,
-// for example, in prepaid schemes. Also, this method might be called from the Rest API,
-// as it is listed in the billing requirements.
-func (m *Monitor) processSuspendedChannels() error {
+func (m *Monitor) VerifySuspendedChannelsAndTryToTerminate() error {
 	query := `
 		SELECT channels.id::text
 		FROM channels
-		  LEFT JOIN offerings offer ON channels.offering = offer.id
+			LEFT JOIN offerings offer ON channels.offering = offer.id
 		
 		WHERE
-		  channels.service_status = 'suspended' AND
-		    channels.service_changed_time + (offer.max_suspended_time * INTERVAL '1 second') < now()`
+			channels.service_status = 'suspended' AND
+			channels.service_changed_time + (offer.max_suspended_time * INTERVAL '1 second') < now()`
 
-	return m.processEachChannel(query, m.suspendService)
+	return m.processEachChannel(query, m.terminateService)
 }
 
-
+func (m *Monitor) processRound() error {
+	return m.callChecksAndReportErrorIfAny(
+		m.VerifySecondsBasedChannels,
+		m.VerifyUnitsBasedChannels,
+		m.VerifyChannelsForInactivity,
+		m.VerifySuspendedChannelsAndTryToUnsuspend,
+		m.VerifySuspendedChannelsAndTryToTerminate)
+}
 
 func (m *Monitor) suspendService(uuid string) error {
-	// todo: [call the task]
+	// todo: [call the task] [simon]
 	return nil
 }
 
 func (m *Monitor) terminateService(uuid string) error {
-	// todo: [call the task]
+	// todo: [call the task] [simon]
 	return nil
 }
 
-func (m *Monitor) continueService(uuid string) error {
-	// todo: [call the task]
+func (m *Monitor) unsuspendService(uuid string) error {
+	// todo: [call the task] [simon]
 	return nil
 }
 
@@ -199,10 +207,18 @@ func (m *Monitor) processEachChannel(query string, processor func(string) error)
 		return err
 	}
 
-	channelUUID := ""
+	if m.testsSelectedChannelsIDs != nil {
+		m.testsSelectedChannelsIDs = make([]string, 0)
+	}
+
 	for rows.Next() {
+		channelUUID := ""
 		rows.Scan(&channelUUID)
 		processor(channelUUID)
+
+		if m.testsSelectedChannelsIDs != nil {
+			m.testsSelectedChannelsIDs = append(m.testsSelectedChannelsIDs, channelUUID)
+		}
 	}
 
 	return nil

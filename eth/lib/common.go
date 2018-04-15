3782b054758c5c193f12e36db4ed833c0d31c19a
Dima Chizhevsky <dima.chizhevsky@gmail.com>
2018-04-15 19:12:02 +0000

    1. implemented GetTransactionByHash; 2 tests for GetTransactionByHash and getTransactionReceipt

--- eth/lib/common.go
+++ eth/lib/common.go
@@ -1,9 +1,5 @@
 package lib
 
-import (
-	"strings"
-)
-
 // This module provides low-level methods for accessing common ethereum info.
 // For detailed API description, please refer to:
 // https://ethereumbuilders.gitbooks.io/guide/content/en/ethereum_json_rpc.html
@@ -21,8 +17,6 @@ func (e *EthereumClient) GetGasPrice() (*GasPriceAPIResponse, error) {
 	return response, e.fetch("eth_gasPrice", "", response)
 }
 
-//---------------------------------------------------------------------------------------------------------------------
-
 type BlockNumberAPIResponse GasPriceAPIResponse
 
 // GetBlockNumber returns the number of most recent block in blockchain.
@@ -33,23 +27,50 @@ func (e *EthereumClient) GetBlockNumber() (*BlockNumberAPIResponse, error) {
 	return response, e.fetch("eth_blockNumber", "", response)
 }
 
-//---------------------------------------------------------------------------------------------------------------------
-
 type TransactionReceiptAPIResponse struct {
 	apiResponse
-	Result string `json:"result"`
+	Result struct {
+		TransactionHash   string   `json:"transactionHash"`
+		TransactionIndex  string   `json:"transactionIndex"`
+		BlockHash         string   `json:"blockHash"`
+		BlockNumber       string   `json:"blockNumber"`
+		GasUsed           string   `json:"gasUsed"`
+		CumulativeGasUsed string   `json:"cumulativeGasUsed"`
+		ContractAddress   string   `json:"contractAddress"`
+		Logs              []string `json:"logs"`
+		Status            string   `json:"status"`
+		LogsBloom         string   `json:"logsBloom"`
+	} `json:"result"`
 }
 
 // GetTransactionReceipt returns receipt of the transaction,
 // specified by the hash.
 // https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_gettransactionreceipt
 func (e *EthereumClient) GetTransactionReceipt(hash string) (*TransactionReceiptAPIResponse, error) {
-	if len(hash) > 2 {
-		if strings.ToLower(hash[:2]) != "0x" {
-			hash = "0x" + hash
-		}
-	}
-
 	response := &TransactionReceiptAPIResponse{}
 	return response, e.fetch("eth_getTransactionReceipt", `"`+hash+`"`, response)
+}
+
+type TransactionByHashAPIResponse struct {
+	apiResponse
+	Result struct {
+		Gas              string `json:"gas"`
+		GasPrice         string `json:"gasPrice"`
+		Hash             string `json:"hash"`
+		BlockNumber      string `json:"blockNumber"`
+		Value            string `json:"value"`
+		From             string `json:"from"`
+		Nonce            string `json:"nonce"`
+		BlockHash        string `json:"blockHash"`
+		Input            string `json:"input"`
+		To               string `json:"to"`
+		TransactionIndex string `json:"transactionIndex"`
+	} `json:"result"`
+}
+
+// GetTransactionByHash returns the information about a transaction requested by transaction hash.
+// https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_gettransactionbyhash
+func (e *EthereumClient) GetTransactionByHash(hash string) (*TransactionByHashAPIResponse, error) {
+	response := &TransactionByHashAPIResponse{}
+	return response, e.fetch("eth_getTransactionByHash", `"`+hash+`"`, response)
 }
--- eth/lib/common_test.go
+++ eth/lib/common_test.go
@@ -3,9 +3,12 @@
 package lib
 
 import (
-	"testing"
-
+	"encoding/json"
 	"github.com/privatix/dappctrl/eth/lib/tests"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"testing"
 )
 
 func TestGasPriceFetching(t *testing.T) {
@@ -32,4 +35,51 @@ func TestBlockNumberFetching(t *testing.T) {
 	if response.Result == "" {
 		t.Fatal("Unexpected response received")
 	}
+}
+
+func TestTransactionReceipt(t *testing.T) {
+	transactionHash := getContractTransactionHash()
+	node := tests.GethEthereumConfig().Geth
+	client := NewEthereumClient(node.Host, node.Port)
+	response, err := client.GetTransactionReceipt(transactionHash)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.Result.TransactionHash != transactionHash {
+		t.Fatal("Transaction hashes are not equal.")
+	}
+}
+
+func TestTransactionByHash(t *testing.T) {
+	transactionHash := getContractTransactionHash()
+	node := tests.GethEthereumConfig().Geth
+	client := NewEthereumClient(node.Host, node.Port)
+	response, err := client.GetTransactionByHash(transactionHash)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response.Result.Hash != transactionHash {
+		t.Fatal("Transaction hashes are not equal.")
+	}
+}
+
+func getContractTransactionHash() string {
+	truffleAPI := tests.GethEthereumConfig().TruffleAPI
+	apiResponse, err := http.Get(truffleAPI.Interface() + "/getPSC")
+	if err != nil || apiResponse.StatusCode != 200 {
+		log.Fatal("Can't fetch PSC address. It seems that test environment is broken.")
+	}
+
+	body, err := ioutil.ReadAll(apiResponse.Body)
+	defer apiResponse.Body.Close()
+	if err != nil {
+		log.Fatal("Can't read apiResponse body. It seems that test environment is broken.")
+	}
+
+	data := make(map[string]interface{})
+	json.Unmarshal(body, &data)
+
+	return data["contract"].(map[string]interface{})["transactionHash"].(string)
 }

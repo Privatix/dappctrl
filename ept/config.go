b358000139192973ab4fe66e94a7b77ba2322876
bik <dzeckelev@gmail.com>
2018-05-07 21:55:02 +0700

    added default config and  parameters normalization

--- ept/config.go
+++ ept/config.go
@@ -12,7 +12,38 @@ import (
 
 const (
 	filePerm = 0644
+	autogen  = "autogen"
 )
+
+const (
+	nameProto        = "proto"
+	nameCipher       = "cipher"
+	namePingRestart  = "ping-restart"
+	namePing         = "ping"
+	nameConnectRetry = "connect-retry"
+	nameCaData       = "caData"
+	nameCompLZO      = "comp-lzo"
+)
+
+const (
+	defaultProto         = "tcp"
+	defaultCipher        = "AES-256-CBC"
+	defaultServerAddress = "127.0.0.1"
+	defaultServerPort    = "443"
+	defaultPingRestart   = "10"
+	defaultPing          = "10"
+	defaultConnectRetry  = "2 120"
+)
+
+var defaultConfig = &Config{
+	Proto:         defaultProto,
+	Cipher:        defaultCipher,
+	ServerAddress: defaultServerAddress,
+	Port:          defaultServerPort,
+	PingRestart:   defaultPingRestart,
+	Ping:          defaultPing,
+	ConnectRetry:  defaultConnectRetry,
+}
 
 // Config OpenVpn client model config
 type Config struct {
@@ -25,7 +56,6 @@ type Config struct {
 	ConnectRetry  string
 	Ca            string
 	CompLZO       string
-	Keepalive     string // todo : may be needed in the future [maxim]
 }
 
 // New returns config object
@@ -34,7 +64,8 @@ func New(srvAddr, srvPort string, additionalParams []byte) (*Config, error) {
 		return nil, ErrInput
 	}
 
-	if err := util.ValidateFormat(util.FormatNetworkPort, srvPort); err != nil {
+	if err := util.ValidateFormat(util.FormatNetworkPort,
+		srvPort); err != nil {
 		return nil, err
 	}
 
@@ -51,22 +82,24 @@ func New(srvAddr, srvPort string, additionalParams []byte) (*Config, error) {
 
 	for key, val := range params {
 		switch key {
-		case "proto":
+		case nameProto:
 			config.Proto = val
-		case "cipher":
+		case nameCipher:
 			config.Cipher = val
-		case "ping-restart":
+		case namePingRestart:
 			config.PingRestart = val
-		case "ping":
+		case namePing:
 			config.Ping = val
-		case "connect-retry":
+		case nameConnectRetry:
 			config.ConnectRetry = val
-		case "caData":
+		case nameCaData:
 			config.Ca = val
-		case "comp-lzo":
-			config.CompLZO = "comp-lzo"
+		case nameCompLZO:
+			config.CompLZO = nameCompLZO
 		}
 	}
+
+	//config.normalize()
 
 	return config, nil
 
@@ -76,7 +109,8 @@ func New(srvAddr, srvPort string, additionalParams []byte) (*Config, error) {
 func (c *Config) GetText(tpl string) (string, error) {
 	t := template.New("config")
 
-	t, err := t.Parse(tpl)
+	t, err := t.Funcs(
+		template.FuncMap{autogen: autogenFu}).Parse(tpl)
 	if err != nil {
 		return "", err
 	}
@@ -98,4 +132,38 @@ func (c *Config) SaveToFile(destPath string) error {
 	}
 
 	return ioutil.WriteFile(destPath, []byte(str), filePerm)
+}
+
+func (c *Config) normalize() {
+	if c.Proto == "" {
+		c.Proto = defaultConfig.Proto
+	}
+
+	if c.Cipher == "" {
+		c.Cipher = defaultConfig.Cipher
+	}
+
+	if c.ServerAddress == "" {
+		c.ServerAddress = defaultConfig.ServerAddress
+	}
+
+	if c.Port == "" {
+		c.Port = defaultConfig.Port
+	}
+
+	if c.PingRestart == "" {
+		c.PingRestart = defaultConfig.PingRestart
+	}
+
+	if c.Ping == "" {
+		c.Ping = defaultConfig.Ping
+	}
+
+	if c.ConnectRetry == "" {
+		c.ConnectRetry = defaultConfig.ConnectRetry
+	}
+}
+
+func autogenFu() string {
+	return " # autogenerate option"
 }

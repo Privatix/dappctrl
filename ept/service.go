47cd23ff3b073f361200c53eaf2d013e672360f3
bik <dzeckelev@gmail.com>
2018-05-15 01:38:15 +0700

    code style fix

--- ept/service.go
+++ ept/service.go
@@ -1,11 +1,9 @@
 package ept
 
 import (
-	"encoding/json"
 	"sync"
 	"time"
 
-	"github.com/xeipuuv/gojsonschema"
 	"gopkg.in/reform.v1"
 
 	"github.com/privatix/dappctrl/data"
@@ -307,42 +305,4 @@ func (s *Service) find(done chan bool, errC chan error,
 		return
 	case errC <- err:
 	}
-}
-
-func validMsg(schema []byte, msg Message) bool {
-	sch := gojsonschema.NewBytesLoader(schema)
-	loader := gojsonschema.NewGoLoader(msg)
-
-	result, err := gojsonschema.Validate(sch, loader)
-	if err != nil || !result.Valid() || len(result.Errors()) != 0 {
-		return false
-	}
-	return true
-}
-
-func fillMsg(o *obj, paymentReceiverAddress, serviceEndpointAddress string,
-	conf map[string]string) (*Message, error) {
-
-	if o.prod.OfferAccessID == nil {
-		return nil, ErrProdOfferAccessID
-	}
-
-	return &Message{
-		TemplateHash:           *o.prod.OfferAccessID,
-		Username:               o.ch.ID,
-		Password:               o.ch.Password,
-		PaymentReceiverAddress: paymentReceiverAddress,
-		ServiceEndpointAddress: serviceEndpointAddress,
-		AdditionalParams:       conf,
-	}, nil
-}
-
-func config(confByte []byte) (map[string]string, error) {
-	var conf map[string]string
-
-	if err := json.Unmarshal(confByte, &conf); err != nil {
-		return nil, err
-	}
-
-	return conf, nil
 }

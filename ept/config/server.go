ffae15b1653d59c14d8fae391656f1691c022d79
bik <dzeckelev@gmail.com>
2018-05-15 01:32:37 +0700

    findTemp fix

--- ept/config/server.go
+++ ept/config/server.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"context"
 	"os"
-	"path/filepath"
 	"strings"
 	"time"
 
@@ -33,16 +32,20 @@ type PushConfigReq struct {
 	password string
 	confPath string
 	caPath   string
+	keys     []string
+	retrySec int64
 }
 
 // NewPushConfigReq fills the request structure
 func NewPushConfigReq(username, password, confPath,
-	caPath string) *PushConfigReq {
+	caPath string, keys []string, retrySec int64) *PushConfigReq {
 	return &PushConfigReq{
 		username: username,
 		password: password,
 		confPath: confPath,
 		caPath:   caPath,
+		keys:     keys,
+		retrySec: retrySec,
 	}
 }
 
@@ -98,47 +101,6 @@ func parseConfig(filePath string,
 	return results, nil
 }
 
-func delDup(keys []string) map[string]bool {
-	// delete duplicates
-	keyMap := make(map[string]bool)
-	for _, key := range keys {
-		keyMap[strings.TrimSpace(key)] = true
-	}
-	return keyMap
-}
-
-func searchCa(paths []string) (string, string, bool) {
-	for _, filePath := range paths {
-		cert, err := ParseCertFromFile(filePath)
-		if err == nil {
-			return cert, filePath, true
-		}
-	}
-
-	return "", "", false
-}
-
-func fillCa(results map[string]string, filePath string) error {
-	// check ca key
-	ca := results[caNameFromConfig]
-	if ca == "" {
-		return ErrCertNotExist
-	}
-
-	// absolute path
-	absPath := filepath.Join(filepath.Dir(filePath), ca)
-
-	cert, certPath, found := searchCa([]string{ca, absPath})
-	if !found {
-		return ErrCertNotFound
-	}
-
-	results[caData] = cert
-	results[caPathName] = certPath
-
-	return nil
-}
-
 func parseLine(keys map[string]bool,
 	line string) (string, string, bool) {
 	str := strings.TrimSpace(line)
@@ -183,13 +145,12 @@ func parseLine(keys map[string]bool,
 // The timeout in seconds between attempts to send data to the server must
 // be specified in variable retrySec
 func PushConfig(ctx context.Context, sessSrvConfig *srv.Config,
-	logger *util.Logger, in *PushConfigReq, keys []string,
-	retrySec int64) error {
-	if retrySec <= 0 || logger == nil || sessSrvConfig == nil {
+	logger *util.Logger, in *PushConfigReq) error {
+	if in.retrySec <= 0 || logger == nil || sessSrvConfig == nil {
 		return ErrInput
 	}
 
-	args, err := productArgs(in.confPath, in.caPath, keys)
+	args, err := productArgs(in.confPath, in.caPath, in.keys)
 	if err != nil {
 		return err
 	}
@@ -217,7 +178,7 @@ func PushConfig(ctx context.Context, sessSrvConfig *srv.Config,
 				logger.Warn("Failed to push app config to"+
 					" dappctrl. Original error: %s", err)
 				time.Sleep(time.Second *
-					time.Duration(retrySec))
+					time.Duration(in.retrySec))
 				continue
 			}
 			pushed = true
@@ -226,37 +187,4 @@ func PushConfig(ctx context.Context, sessSrvConfig *srv.Config,
 	}
 
 	return nil
-}
-
-func push(ctx context.Context, errC chan error, req *pushReq) {
-	err := sesssrv.Post(req.sessSrvConfig,
-		req.username, req.password, sesssrv.PathProductConfig,
-		req.args, nil)
-
-	select {
-	case <-ctx.Done():
-		return
-	case errC <- err:
-	}
-}
-
-func productArgs(confPath, caPath string,
-	keys []string) (sesssrv.ProductArgs, error) {
-	result := sesssrv.ProductArgs{}
-
-	conf, err := ServerConfig(confPath, false, keys)
-	if err != nil {
-		return result, err
-	}
-
-	ca, err := ParseCertFromFile(caPath)
-	if err != nil {
-		return result, err
-	}
-
-	conf[caData] = ca
-
-	result.Config = conf
-
-	return result, nil
 }

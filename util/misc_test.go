04388940af8520ecc2d67e90b338c2bc26331404
Constantin S. Pan <kvapen@gmail.com>
2018-05-17 16:38:56 +0300

    Add misc.GenChannelID function based on getKey() implementation in the contract

---
+++ util/misc_test.go
@@ -0,0 +1,22 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+func TestGenChannelID(t *testing.T) {
+	client := common.HexToAddress("0xa")
+	agent := common.HexToAddress("0xb")
+	var block uint32 = 0xc
+	offering := common.HexToHash("0xd")
+
+	// Got the expected value by calling getKey() from truffle console like this:
+	// psc.getKey('0xa', '0xb', 0xc, '\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x0d')
+	expected := common.HexToHash("0x2c93cebfce906a0f7aab3b6f6537ae1a8c3621c9f1bc749017da38621b2ea3a0")
+	actual := GenChannelID(client, agent, block, offering)
+	if actual != expected {
+		t.Fatalf("wrong channel id generated: got %s, expected %s", actual.Hex(), expected.Hex())
+	}
+}

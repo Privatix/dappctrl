2e83248112c3bc4c1dfe0ecc7196bc1c4a36a63f
Andrew Andreev <for.drew@gmail.com>
2018-05-25 12:09:36 +0300

    extract README.md

--- monitor/collect.go
+++ monitor/collect.go
@@ -50,54 +50,6 @@ var offeringRelatedEvents = hexesToHashes(
 	eth.EthOfferingPoppedUp,
 )
 
-/*
-Monitor performs Log collecting periodically.
-
-Several most recent blocks on the blockchain are considered "unreliable" (the
-relevant setting is "eth.min.confirmations").
-
-Let A = last processed block number
-    Z = most recent block number on the blockchain
-    C = the min confirmations setting
-    F = the fresh offerings setting
-
-Thus the range of interest for agent and client logs Ri = [A + 1, Z - C],
-and for offering it is:
-
-    if F > 0 Ro = Ri ∩ [Z - C - F, +inf)
-    else Ro = Ri
-
-These are the rules for filtering logs on the blockchain:
-
-1. Events for agent
-   - From: A + 1
-   - To:   Z - C
-   - Topics[0]: any
-   - Topics[1]: one of accounts with in_use = true
-
-2. Events for client
-   - From: A + 1
-   - To:   Z - C
-   - Topics[0]: one of these hashes
-     - LogChannelCreated
-     - LogChannelToppedUp
-     - LogChannelCloseRequested
-     - LogCooperativeChannelClose
-     - LogUnCooperativeChannelClose
-   - Topics[2]: one of the accounts with in_use = true
-
-3. Offering events
-   - From: max(A + 1, Z - C - F) if F > 0
-   - From: A + 1 if F == 0
-   - To:   Z - C
-   - Topics[0]: one of these hashes
-     - LogOfferingCreated
-     - LogOfferingDeleted
-     - LogOfferingPopedUp
-   - Topics[1]: not one of the accounts with in_use = true
-   - Topics[2]: one of the accounts with in_use = true
-*/
-
 // collect requests new logs and puts them into the database.
 func (m *Monitor) collect(ctx context.Context) {
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second) // FIXME: hardcoded duration

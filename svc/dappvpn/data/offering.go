9b020403371332b96fa6f5ceca8c21c20b1b3294
Simon Prykhodko <semion.ababo@gmail.com>
2018-09-17 15:53:21 +0200

    Support default and floating point rates for traffic shaping.

--- svc/dappvpn/data/offering.go
+++ svc/dappvpn/data/offering.go
@@ -2,6 +2,6 @@ package data
 
 // OfferingParams is an offering additional parameters for VPN.
 type OfferingParams struct {
-	MinUploadMbits   int `json:"minUploadMbits"`
-	MinDownloadMbits int `json:"minDownloadMbits"`
+	MinUploadMbits   float32 `json:"minUploadMbits"`
+	MinDownloadMbits float32 `json:"minDownloadMbits"`
 }
--- tc/tc_windows.go
+++ tc/tc_windows.go
@@ -12,7 +12,7 @@ func NewConfig() *Config {
 // SetRateLimit sets a rate limit for a given client IP address on a given
 // network interface.
 func (tc *TrafficControl) SetRateLimit(
-	iface, clientIP string, upMbps, downMbps int) error {
+	iface, clientIP string, upMbps, downMbps float32) error {
 	return nil
 }
 

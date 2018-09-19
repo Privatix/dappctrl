a56f0f70fdc8ba971a65aa7dadacef88796ee7c0
drew2a <for.drew@gmail.com>
2018-09-19 19:12:08 +0400

    remove echo

--- scripts/create_database.sh
+++ scripts/create_database.sh
@@ -3,6 +3,3 @@ DAPPCTRL=github.com/privatix/dappctrl
 DAPPCTRL_DIR=$HOME/go/src/${DAPPCTRL}
 
 psql -U postgres -f "${DAPPCTRL_DIR}/data/settings.sql"
-
-echo
-echo "Database has been created successfully."

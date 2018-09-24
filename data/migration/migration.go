95e397b04f6007fd7e11e140edb895be4f8463ba
Urmat Bozov <urmat@privatix.io>
2018-09-24 21:22:09 +0600

    commands to downgrade the database schema and display version

--- data/migration/migration.go
+++ data/migration/migration.go
@@ -14,35 +14,56 @@ func exec(query string, tx *sql.Tx) error {
 const minVersion = int64(0)
 const maxVersion = int64((1 << 63) - 1)
 
-// Update executes migration scripts.
-func Update(connStr string) error {
+// Version returns the version of the database schema.
+func Version(connStr string) (int64, error) {
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return 0, err
+	}
+	defer db.Close()
+	return goose.GetDBVersion(db)
+}
+
+// Migrate executes migration scripts.
+func Migrate(connStr string, version int64) error {
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return err
 	}
 	defer db.Close()
 
-	return executeMigrationScripts(db)
+	if version == 0 {
+		migrations, err := goose.CollectMigrations(".", minVersion, maxVersion)
+		if err != nil {
+			return err
+		}
+
+		last, err := migrations.Last()
+		if err != nil {
+			return err
+		}
+		version = last.Version
+	}
+
+	return executeMigrationScripts(db, version)
 }
 
-func executeMigrationScripts(db *sql.DB) error {
+func executeMigrationScripts(db *sql.DB, version int64) error {
 	current, err := goose.GetDBVersion(db)
 	if err != nil {
 		return err
 	}
 
-	migrations, err := goose.CollectMigrations(".", minVersion, maxVersion)
-	if err != nil {
-		return err
+	// upgrade database schema
+	if version > current {
+		if err := goose.UpTo(db, ".", version); err != nil {
+			return err
+		}
 	}
 
-	last, err := migrations.Last()
-	if err != nil {
-		return err
-	}
-
-	if last.Version > current {
-		if err := goose.Up(db, "."); err != nil {
+	// downgrade database schema
+	if version < current {
+		if err := goose.DownTo(db, ".", version); err != nil {
 			return err
 		}
 	}

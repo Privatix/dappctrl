6ae0e97175801acacacb2a42f01957cf7d396bee
Urmat Bozov <urmat@privatix.io>
2018-09-24 21:22:09 +0600

    commands to downgrade the database schema and display version

--- data/command.go
+++ data/command.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/privatix/dappctrl/data/migration"
@@ -12,9 +13,18 @@ import (
 	reform "gopkg.in/reform.v1"
 )
 
+type cmdFlag int
+
+// cmdFlags enum
+const (
+	ConnectionString cmdFlag = iota
+	Version
+)
+
 // ExecuteCommand executes commands to manage database
-// db-migrate - command for execute migration scripts
-// db-init-data - command for initialize database by default values.
+// db-migrate - command to execute migration scripts
+// db-init-data - command to initialize database by default values
+// db-version - command to print the version of the database schema.
 func ExecuteCommand(args []string) error {
 	if len(args) == 0 {
 		return nil
@@ -22,30 +32,48 @@ func ExecuteCommand(args []string) error {
 
 	switch args[0] {
 	case "db-migrate":
-		connStr := getConnectionString(args)
-		if err := migration.Update(connStr); err != nil {
+		m := readFlags(args)
+		connStr := m[ConnectionString]
+		version, _ := strconv.ParseInt(m[Version], 10, 64)
+		if err := migration.Migrate(connStr, version); err != nil {
 			panic(fmt.Sprintf("failed to run migration %s", err))
 		}
 		os.Exit(0)
 	case "db-init-data":
-		connStr := getConnectionString(args)
+		connStr := readFlags(args)[ConnectionString]
 		if err := initData(connStr); err != nil {
 			panic(fmt.Sprintf("failed to init database %s", err))
 		}
+		os.Exit(0)
+	case "db-version":
+		connStr := readFlags(args)[ConnectionString]
+		version, err := migration.Version(connStr)
+		if err != nil {
+			msg := "failed to print database schema version"
+			panic(fmt.Sprintf("%s %s", msg, err))
+		}
+		fmt.Println("database schema version:", version)
 		os.Exit(0)
 	}
 	return nil
 }
 
-func getConnectionString(args []string) string {
+func readFlags(args []string) map[cmdFlag]string {
 	connStr := flag.String("conn", "", "Database connection string")
+	version := flag.Int("version", 0, "Migrate to version")
 
 	flag.CommandLine.Parse(args[1:])
 
 	if *connStr == "" {
 		panic(errors.New("connection string is not detected"))
 	}
-	return *connStr
+
+	m := make(map[cmdFlag]string)
+
+	m[ConnectionString] = *connStr
+	m[Version] = strconv.Itoa(*version)
+
+	return m
 }
 
 func initData(connStr string) error {

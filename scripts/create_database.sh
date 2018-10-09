99c3353e9a26bc5d6bf1071fc1ab979934f8bd63
drew2a <for.drew@gmail.com>
2018-10-09 11:54:31 +0300

    move migration scripts

--- scripts/create_database.sh
+++ scripts/create_database.sh
@@ -3,3 +3,6 @@ DAPPCTRL=github.com/privatix/dappctrl
 DAPPCTRL_DIR=$HOME/go/src/${DAPPCTRL}
 
 psql -U postgres -f "${DAPPCTRL_DIR}/data/settings.sql"
+dappctrl db-migrate -conn 'host=localhost sslmode=disable dbname=dappctrl user=postgres port=5432'
+dappctrl db-init-data -conn 'host=localhost sslmode=disable dbname=dappctrl user=postgres port=5432'
+

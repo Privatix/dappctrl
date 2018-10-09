311966071080fea0b1b4e899c09e5d3c4ebb4f0f
drew2a <for.drew@gmail.com>
2018-10-09 12:06:24 +0300

    revert script to old version

--- scripts/create_database.sh
+++ scripts/create_database.sh
@@ -3,6 +3,6 @@ DAPPCTRL=github.com/privatix/dappctrl
 DAPPCTRL_DIR=$HOME/go/src/${DAPPCTRL}
 
 psql -U postgres -f "${DAPPCTRL_DIR}/data/settings.sql"
-dappctrl db-migrate -conn 'host=localhost sslmode=disable dbname=dappctrl user=postgres port=5432'
-dappctrl db-init-data -conn 'host=localhost sslmode=disable dbname=dappctrl user=postgres port=5432'
+psql -U postgres -d dappctrl -f "${DAPPCTRL_DIR}/data/schema.sql"
+psql -U postgres -d dappctrl -f "${DAPPCTRL_DIR}/data/prod_data.sql"
 

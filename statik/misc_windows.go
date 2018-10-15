671968e1468db473e753764c077f09b525016716
Andrei Tseitlin <andrei@privatix.io>
2018-10-16 02:43:47 +0300

    fix statik for win

---
+++ statik/misc_windows.go
@@ -0,0 +1,38 @@
+package statik
+
+import (
+	"io/ioutil"
+
+	"github.com/rakyll/statik/fs"
+)
+
+//go:generate .\pkgList\gen_win.bat
+//go:generate go run ../tool/copy_dbscripts/copy.go
+//go:generate cmd /C del /F /Q statik.go
+//go:generate statik -f -src=. -dest=..
+
+// File paths.
+const (
+	EndpointJSONSchema = "/templates/ept.json"
+)
+
+// ReadFile reads a file content from the embedded filesystem.
+func ReadFile(name string) ([]byte, error) {
+	fs, err := fs.New()
+	if err != nil {
+		return nil, ErrOpenFS
+	}
+
+	file, err := fs.Open(name)
+	if err != nil {
+		return nil, ErrOpenFile
+	}
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, ErrReadFile
+	}
+
+	return data, nil
+}

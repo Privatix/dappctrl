b594fb4d5ba9bb7b50aa605e0675d047f4fd39d4
drew2a <for.drew@gmail.com>
2018-11-13 10:34:43 +0300

    speedup events recieve

--- data/prod_data.sql
+++ data/prod_data.sql
@@ -18,7 +18,7 @@ VALUES ('eth.min.confirmations',
 
 INSERT INTO settings (key, value, permissions, description, name)
 VALUES ('eth.event.freshblocks',
-        '11520',
+        '1000',
         2,
         'defines number of latest ethereum blocks to retrieve.' ||
         ' If eth.event.freshblocks is null or zero then all events' ||
@@ -27,7 +27,7 @@ VALUES ('eth.event.freshblocks',
 
 INSERT INTO settings (key, value, permissions, description, name)
 VALUES ('eth.event.blocklimit',
-        '80',
+        '500',
         2,
         'maximum number of ethereum blocks that is used to scan' ||
         ' for new events. It is used as pagination mechanism while' ||

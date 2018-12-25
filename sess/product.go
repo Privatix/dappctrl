5851efcc27bb42d82f4a8d55e5670457a5597c7c
Simon Prykhodko <semion.ababo@gmail.com>
2018-12-25 12:21:36 +0200

    Remove heartbeat handler in the session server

--- sess/product.go
+++ sess/product.go
@@ -1,7 +1,6 @@
 package sess
 
 import (
-	"database/sql"
 	"encoding/json"
 	"net"
 	"strings"
@@ -114,53 +113,4 @@ func (h *Handler) SetProductConfig(
 	}
 
 	return nil
-}
-
-// Heartbeat commands.
-const (
-	HeartbeatStart = "start"
-	HeartbeatStop  = "stop"
-)
-
-// HeartbeatResult is a result of heartbeat request.
-type HeartbeatResult struct {
-	Channel string `json:"channel,omitempty"`
-	Command string `json:"command,omitempty"`
-}
-
-// HandleHeartbeat receives adapter heartbeat and returns management commands.
-func (h *Handler) HandleHeartbeat(
-	product, productPassword string) (*HeartbeatResult, error) {
-	logger := h.logger.Add(
-		"method", "HandleProductHeartbeat", "product", product)
-
-	logger.Debug("adapter heartbeat")
-
-	_, err := h.checkProductPassword(logger, product, productPassword)
-	if err != nil {
-		return nil, err
-	}
-
-	var ch data.Channel
-	err = h.db.SelectOneTo(&ch, `
-		 JOIN offerings ON offering = offerings.id
-		WHERE product = $1 AND service_status IN
-		        ('activating', 'suspending', 'terminating')
-		ORDER BY service_changed_time DESC`, product)
-	if err != nil && err != sql.ErrNoRows {
-		logger.Error(err.Error())
-		return nil, ErrInternal
-	}
-
-	var result HeartbeatResult
-	if err == nil {
-		result.Channel = ch.ID
-		if ch.ServiceStatus == data.ServiceActivating {
-			result.Command = HeartbeatStart
-		} else {
-			result.Command = HeartbeatStop
-		}
-	}
-
-	return &result, nil
 }

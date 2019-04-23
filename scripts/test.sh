550669672aa86ebd7fa666f680cdce88c77a54c4
drew2a <for.drew@gmail.com>
2019-04-23 18:46:22 +0700

    add travis
    add travis
    
    
    fix script
    
    
    add postgresql 10
    
    
    try to build
    
    
    add global
    
    
    add services
    
    
    try to use port
    
    
    try to remove addon
    
    
    try 5432
    
    
    change postgresql version
    
    
    upgrade postgresql version
    
    
    fix test.sh
    
    
    db host = localhost
    
    
    fix test run
    
    
    go test
    
    
    -p 1
    
    
    try once again
    
    
    ad space
    
    
    add noethtest

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,10 +3,4 @@
 MY_PATH="`dirname \"$0\"`" # relative bash file path
 DAPPCTRL_DIR="`( cd \"$MY_PATH/..\" && pwd )`"  # absolutized and normalized dappctrl path
 
-CONF_FILE="${DAPPCTRL_DIR}/dappctrl-test.config.json"
-LOCAL_CONF_FILE=$HOME/dappctrl-test.config.json
-DB_IP=10.16.194.21
-STRESS_JOBS=1000
-
-jq ".DB.Conn.host=\"$DB_IP\" | .JobTest.StressJobs=$STRESS_JOBS" "${CONF_FILE}" > "${LOCAL_CONF_FILE}"
-go test ${DAPPCTRL}/... -p=1 -config="${LOCAL_CONF_FILE}"
+go test -v "${DAPPCTRL_DIR}/..." -config="${DAPPCTRL_DIR}/dappctrl-test.config.json" -tags=noethtest -p=1

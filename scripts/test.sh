ec8c26cec9e9011eff87acf0c591b22853075af0
furkhat <vailodf@gmail.com>
2019-04-24 14:05:52 +0600

    display errors and logs on tests failure only

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,4 +3,4 @@
 MY_PATH="`dirname \"$0\"`" # relative bash file path
 DAPPCTRL_DIR="`( cd \"$MY_PATH/..\" && pwd )`"  # absolutized and normalized dappctrl path
 
-go test -v "${DAPPCTRL_DIR}/..." -config="${DAPPCTRL_DIR}/dappctrl-test.config.json" -tags=noethtest -p=1
+go test "${DAPPCTRL_DIR}/..." -config="${DAPPCTRL_DIR}/dappctrl-test.config.json" -tags=noethtest -vv -p=1

9f861e83ead53600cc94d3d74a1aa569bf44605a
drew2a <for.drew@gmail.com>
2019-08-01 12:35:09 +0200

    revert default gas price

--- data/prod_data.sql
+++ data/prod_data.sql
@@ -69,7 +69,7 @@ DO NOTHING;
 
 INSERT INTO settings (key, value, permissions, description, name)
 VALUES ('eth.default.gasprice',
-        '10000000000',
+        '20000000000',
         2,
         'Default GAS price for transactions.',
         'default gas price')

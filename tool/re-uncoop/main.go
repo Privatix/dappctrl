d41bc4895ebaf70493e82025b625e4b56010844e
furkhat <vailodf@gmail.com>
2019-08-14 17:31:03 +0600

    re-uncoop tool

---
+++ tool/re-uncoop/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/privatix/dappctrl/data"
+	"github.com/privatix/dappctrl/job"
+	"github.com/privatix/dappctrl/util"
+)
+
+func main() {
+	fconfig := flag.String("config", "dappctrl.config.json", "dappctrl configuration file")
+
+	flag.Parse()
+
+	conf := struct {
+		DB *data.DBConfig
+	}{
+		DB: data.NewDBConfig(),
+	}
+	if err := util.ReadJSONFile(*fconfig, &conf); err != nil {
+		panic(fmt.Sprintf("failed to read configuration: %s", err))
+	}
+
+	db, err := data.NewDB(conf.DB)
+	if err != nil {
+		panic(fmt.Sprintf("failod to make db client: %v", err))
+	}
+
+	query := `UPDATE jobs
+	            SET try_count=0, status=$1, not_before=$2
+			    WHERE type=$3 AND status=$4`
+	if _, err := db.Exec(query, data.JobActive, nil,
+		data.JobClientPreUncooperativeClose, data.JobFailed); err != nil {
+		panic(fmt.Sprintf("failed to re-activate uncoop close jobs: %v", err))
+	}
+
+	jobConf := struct {
+		Job *job.Config
+	}{
+		Job: job.NewConfig(),
+	}
+	if err := util.ReadJSONFile(*fconfig, &jobConf); err != nil {
+		panic(fmt.Sprintf("failed to read configuration: %s", err))
+	}
+	tmp := jobConf.Job.Types[data.JobClientPreUncooperativeClose]
+	tmp.FirstStartDelay = 750000000
+	jobConf.Job.Types[data.JobClientPreUncooperativeClose] = tmp
+	allConf := make(map[string]interface{})
+	if err := util.ReadJSONFile(*fconfig, &allConf); err != nil {
+		panic(fmt.Sprintf("failed to read configuration: %s", err))
+	}
+	allConf["Job"] = jobConf.Job
+	if err := util.WriteJSONFile(*fconfig, "", "	", allConf); err != nil {
+		panic(fmt.Sprintf("failed to write file: %v", err))
+	}
+}
